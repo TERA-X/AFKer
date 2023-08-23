e6cc60d0c57f05290bf4563237651758bd9a5bde
X0RPR0 <57594126+X0RPR0@users.noreply.github.com>
2023-08-23 12:35:27 +0200

    Upload

---
+++ README.md
@@ -0,0 +1,10 @@
+##### :heavy_exclamation_mark: Status :heavy_exclamation_mark:
+Should work on all regions as long as the opcodes are mapped.
+
+# AFKer
+A tera-proxy module that prevents you from getting back to the character select screen.  
+Once activated, the server's AFK check will not kick you out of the game.  
+
+## Usage
+The script is enabled by default and will prevent you from getting logged out when you are standing still for an hour.  
+Simply moving your character will reset the 1 hour timer and make you able to go back to character selection.  
---
+++ index.js
@@ -0,0 +1,25 @@
+// Version 1.2.6
+
+module.exports = function afker(mod) {
+
+	let enabled = true,
+		lasttimemoved = Date.now()
+
+	mod.hook('C_PLAYER_LOCATION', 5, event => {
+		if([0,1,5,6].indexOf(event.type) > -1) // running / walking / jumping / jumping (steep terrain)
+			lasttimemoved = Date.now()
+	})
+
+	mod.hook('C_RETURN_TO_LOBBY', 'raw', () => {
+		if (enabled && Date.now() - lasttimemoved >= 3600000) return false // Prevents you from being logged out after not moving for 1 hour
+	})
+
+	// ################# //
+	// ### Chat Hook ### //
+	// ################# //
+
+	mod.command.add('afk', () => {
+		enabled = !enabled
+		mod.command.message('AFKer ' + (enabled ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'))
+	})
+}
